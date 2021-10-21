e1d867f1bb732a7f48e7dab8c293aa412f211c99
Young-ook Kim <youngook.kim@gmail.com>
2021-10-21 18:29:52 +0900

    fix(lambda): naming rule

--- labels.tf
+++ labels.tf
@@ -1,11 +1,10 @@
 resource "random_pet" "name" {
-  count     = var.enabled ? 1 : 0
   length    = 3
   separator = "-"
 }
 
 locals {
-  name = var.name == null ? (var.enabled ? random_pet.name.0.id : "") : var.name
+  name = var.name == null || var.name == "" ? random_pet.name.id : var.name
   default-tags = merge(
     { "terraform.io" = "managed" },
     { "Name" = local.name },

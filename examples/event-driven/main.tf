eed2be44647030a44dcb2c1b9dcd09b0e86c30e5
Young-ook <youngook.kim@gmail.com>
2021-10-25 16:31:03 +0900

    feat(evnet-driven): ci/cd pipeline (#12)
    * feat(evnet-driven): ci/cd pipeline
    
    * feat(lambda): support s3 artifact repo
    
    * docs(event-driven): updated event-driven example instructions
    
    * fix(event-driven): correct policy_arns variable name

--- examples/event-driven/main.tf
+++ examples/event-driven/main.tf
@@ -28,10 +28,14 @@ resource "aws_lambda_permission" "lambda" {
 
 # lambda
 module "lambda" {
-  source         = "../../"
-  name           = var.name
-  tags           = var.tags
-  lambda_config  = var.lambda_config
+  source = "../../"
+  name   = var.name
+  tags   = var.tags
+  lambda_config = {
+    s3_bucket = module.artifact.bucket.id
+    s3_key    = "lambda_handler.zip"
+    handler   = "lambda_handler.lambda_handler"
+  }
   tracing_config = var.tracing_config
   vpc_config     = var.vpc_config
   policy_arns    = [module.logs.policy_arns["write"]]
@@ -42,4 +46,36 @@ module "logs" {
   source     = "Young-ook/lambda/aws//modules/logs"
   name       = var.name
   log_config = var.log_config
+}
+
+# pipeline
+module "ci" {
+  source = "Young-ook/spinnaker/aws//modules/codebuild"
+  name   = var.name
+  tags   = var.tags
+  environment_config = {
+    image           = "aws/codebuild/standard:4.0"
+    privileged_mode = true
+    environment_variables = {
+      WORKDIR         = "examples/event-driven"
+      PKG             = lookup(var.lambda_config, "package", "lambda_handler.zip")
+      ARTIFACT_BUCKET = module.artifact.bucket.id
+    }
+  }
+  source_config = {
+    type      = "GITHUB"
+    location  = "https://github.com/Young-ook/terraform-aws-lambda.git"
+    buildspec = "examples/event-driven/buildspec.yml"
+    version   = "main"
+  }
+  policy_arns = [
+    module.artifact.policy_arns["write"],
+  ]
+}
+
+module "artifact" {
+  source        = "Young-ook/spinnaker/aws//modules/s3"
+  name          = var.name
+  tags          = var.tags
+  force_destroy = true
 }

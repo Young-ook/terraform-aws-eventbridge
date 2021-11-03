672a16bba6cc8d7b7e6958a86471d81c39ed89a3
Young-ook Kim <youngook.kim@gmail.com>
2021-11-03 13:21:37 +0900

    fix(alarm): null parameter exception handler

--- modules/alarm/default.tf
+++ modules/alarm/default.tf
@@ -1,6 +1,11 @@
 # default variables
 
 locals {
+  alarm_config = var.alarm_config == null ? {} : var.alarm_config
+  metric_query = var.metric_query == null ? [] : var.metric_query
+}
+
+locals {
   default_alarm_config = {
     threshold                             = 1
     treat_missing_data                    = "missing"

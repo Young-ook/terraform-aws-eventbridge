64c3072a6d04aaa5cfa3179321d68de20271d348
youngookkim <youngook.kim@gmail.com>
2022-12-06 19:45:22 -0800

    chore(lambda): migration lambda module (#54)

---
+++ modules/lambda/defaults.tf
@@ -0,0 +1,33 @@
+### default values
+
+module "aws" {
+  source = "Young-ook/spinnaker/aws//modules/aws-partitions"
+}
+
+locals {
+  default_lambda_config = {
+    package                 = null
+    handler                 = "lambda_handler"
+    runtime                 = "python3.8"
+    memory                  = 128
+    timeout                 = 3
+    publish                 = false
+    layers                  = []
+    region                  = module.aws.region.name
+    provisioned_concurrency = -1
+    environment_variables   = {}
+  }
+  default_bucket_config = {
+    s3_bucket         = null
+    s3_key            = null
+    s3_object_version = null
+  }
+  default_vpc_config = {}
+  default_log_config = {
+    name           = format("/aws/lambda/%s", local.name)
+    retention_days = 7
+  }
+  default_tracing_config = {
+    mode = "PassThrough"
+  }
+}
---
+++ modules/lambda/variables.tf
@@ -0,0 +1,49 @@
+### input variables
+
+### network
+variable "vpc" {
+  description = "VPC configuration for function"
+  type        = any
+  default     = {}
+}
+
+### computing/function
+variable "lambda" {
+  description = "Lambda function configuration"
+  type        = any
+  default     = {}
+}
+
+### observability/tracing
+variable "tracing" {
+  description = "Tracing configuration for function"
+  type        = any
+  default     = {}
+}
+
+### observability/logs
+variable "logs" {
+  description = "Log configuration for function"
+  type        = any
+  default     = {}
+}
+
+### security
+variable "policy_arns" {
+  description = "A list of policy ARNs to attach the role for lambda function"
+  type        = list(string)
+  default     = []
+}
+
+### description
+variable "name" {
+  description = "The logical name of the module instance"
+  type        = string
+}
+
+### tags
+variable "tags" {
+  description = "The key-value maps for tagging"
+  type        = map(string)
+  default     = {}
+}

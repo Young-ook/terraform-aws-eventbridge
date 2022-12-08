028ae6863d335ac95a4d3595c9b694e4649b56a2
Young-ook Kim <youngook.kim@gmail.com>
2022-12-08 10:15:37 +0900

    feat(blueprint): code pipeline

---
+++ examples/blueprint/awscp.tf
@@ -0,0 +1,122 @@
+### pipeline/artifact
+resource "random_pet" "petname" {
+  for_each  = { bucket = 3, github = 3 }
+  length    = each.value3
+  separator = "-"
+}
+
+module "artifact" {
+  source        = "Young-ook/sagemaker/aws//modules/s3"
+  version       = "0.2.0"
+  name          = random_pet["bucket"].id
+  tags          = var.tags
+  force_destroy = true
+}
+
+resource "aws_iam_policy" "github" {
+  name        = random_pet["github"].id
+  description = "Allows to run code build"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect"   = "Allow"
+        "Action"   = ["codestar-connections:UseConnection"]
+        "Resource" = aws_codestarconnections_connection.github.arn
+      },
+    ]
+  })
+}
+
+resource "aws_codestarconnections_connection" "github" {
+  name          = random_pet["github"].id
+  provider_type = "GitHub"
+}
+
+module "pipeline" {
+  source  = "Young-ook/eventbridge/aws//modules/pipeline"
+  version = "0.0.7"
+  name    = var.name
+  tags    = var.tags
+  policy_arns = [
+    aws_iam_policy.github.arn,
+    module.artifact.policy_arns.write,
+  ]
+  artifact_config = [{
+    location = module.artifact.bucket.id
+    type     = "S3"
+  }]
+  stage_config = [
+    {
+      name = "Source"
+      actions = [{
+        name             = "Source"
+        category         = "Source"
+        owner            = "AWS"
+        provider         = "CodeStarSourceConnection"
+        version          = "1"
+        output_artifacts = ["source_output"]
+        run_order        = 1
+        configuration = {
+          ConnectionArn    = aws_codestarconnections_connection.github.arn
+          FullRepositoryId = "Young-ook/terraform-aws-eventbridge"
+          BranchName       = "main"
+        }
+      }]
+    },
+    {
+      name = "Build"
+      actions = [{
+        name             = "Build"
+        category         = "Build"
+        owner            = "AWS"
+        provider         = "CodeBuild"
+        version          = "1"
+        input_artifacts  = ["source_output"]
+        output_artifacts = ["build_output"]
+        run_order        = 2
+        configuration = {
+          ProjectName = module.build.project.name
+        }
+      }]
+    },
+  ]
+}
+
+locals {
+  projects = [
+    {
+      name      = "build"
+      buildspec = "examples/blueprint/apps/running/build.yaml"
+      app_path  = "examples/blueprint/apps/running"
+    },
+  ]
+}
+
+module "build" {
+  for_each    = { for proj in local.projects : proj.name => proj }
+  source      = "Young-ook/spinnaker/aws//modules/codebuild"
+  version     = "2.3.6"
+  name        = each.key
+  tags        = var.tags
+  policy_arns = [each.key == "build" ? module.artifact.policy_arns.write : "arn:aws:iam::aws:policy/AdministratorAccess"]
+  project = {
+    source = {
+      type      = "GITHUB"
+      location  = "https://github.com/Young-ook/terraform-aws-eventbridge.git"
+      buildspec = lookup(each.value, "buildspec")
+      version   = "main"
+    }
+    environment = {
+      compute_type    = lookup(each.value, "compute_type", "BUILD_GENERAL1_SMALL")
+      type            = lookup(each.value, "type", "LINUX_CONTAINER")
+      image           = lookup(each.value, "image", "aws/codebuild/standard:4.0")
+      privileged_mode = true
+      environment_variables = {
+        APP_PATH        = lookup(each.value, "app_path")
+        PKG             = "lambda_handler.zip"
+        ARTIFACT_BUCKET = module.artifact.bucket.id
+      }
+    }
+  }
+}

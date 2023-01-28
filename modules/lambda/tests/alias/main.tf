414bfea965b31aefe8553474d64315599de4fbf1
youngookkim <youngook.kim@gmail.com>
2023-01-28 20:35:02 +0900

    feat(lambda): lambda alias (#67)
    * feat(lambda): lambda alias
    
    * chore(git): update git ignore list to ignore zip files from tests
    
    * feat(lambda): add lambda invokation code to tests

---
+++ modules/lambda/tests/alias/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+  }
+}
+
+### application/package
+data "archive_file" "lambda_zip_file" {
+  output_path = join("/", [path.module, "lambda_handler.zip"])
+  source_file = join("/", [path.module, "lambda_function.py"])
+  type        = "zip"
+}
+
+### application/function
+module "main" {
+  source = "../.."
+  lambda = {
+    handler          = "lambda_function.lambda_handler"
+    package          = data.archive_file.lambda_zip_file.output_path
+    source_code_hash = data.archive_file.lambda_zip_file.output_base64sha256
+    publish          = true
+  }
+}
+
+### invoke
+data "aws_lambda_invocation" "invoke" {
+  depends_on    = [module.main]
+  function_name = module.main.function.function_name
+  input = jsonencode({
+    key1 = "value1"
+    key2 = "value2"
+    key3 = "value3"
+  })
+}
+
+resource "test_assertions" "alias_name" {
+  component = "alias_name"
+
+  check "alias_name" {
+    description = "lambda alias name"
+    condition   = can(regex("^lambda", module.main.alias.name))
+  }
+}
---
+++ modules/lambda/tests/defaults/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+  }
+}
+
+### application/package
+data "archive_file" "lambda_zip_file" {
+  output_path = join("/", [path.module, "lambda_handler.zip"])
+  source_file = join("/", [path.module, "lambda_function.py"])
+  type        = "zip"
+}
+
+### application/function
+module "main" {
+  source = "../.."
+  lambda = {
+    handler          = "lambda_function.lambda_handler"
+    package          = data.archive_file.lambda_zip_file.output_path
+    source_code_hash = data.archive_file.lambda_zip_file.output_base64sha256
+  }
+}
+
+### invoke
+data "aws_lambda_invocation" "invoke" {
+  depends_on    = [module.main]
+  function_name = module.main.function.function_name
+  input = jsonencode({
+    key1 = "value1"
+    key2 = "value2"
+    key3 = "value3"
+  })
+}

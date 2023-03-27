74795011822d9893211512e3221e6c00f02552f2
Young-ook Kim <youngook.kim@gmail.com>
2023-03-27 21:07:15 +0900

    fix(pipeline): remove the out-dated pipeline example

--- examples/pipeline/default.auto.tfvars
+++
@@ -1,5 +0,0 @@
-aws_region = "ap-northeast-2"
-tags = {
-  env = "dev"
-}
-log_config = null
--- examples/pipeline/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "github_connection_status" {
-  description = "The CodeStar Connection status. Possible values are PENDING, AVAILABLE and ERROR."
-  value       = aws_codestarconnections_connection.github.connection_status
-}

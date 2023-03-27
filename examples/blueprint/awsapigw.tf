5893936950e1e08b348d33309e331235c85b310a
Young-ook Kim <youngook.kim@gmail.com>
2023-03-27 19:47:16 +0900

    feat(blueprint): add apigw for http api service

---
+++ examples/blueprint/awsapigw.tf
@@ -0,0 +1,52 @@
+### application/apigw
+resource "aws_api_gateway_rest_api" "apigw" {
+  name        = "lambda-apigateway"
+  description = "Managed by Terraform"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  parent_id   = aws_api_gateway_rest_api.apigw.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.apigw.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.apigw.id
+  resource_id             = aws_api_gateway_method.proxy.resource_id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda["httpd"].function.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.apigw.id
+  resource_id   = aws_api_gateway_rest_api.apigw.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id             = aws_api_gateway_rest_api.apigw.id
+  resource_id             = aws_api_gateway_method.proxy_root.resource_id
+  http_method             = aws_api_gateway_method.proxy_root.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda["httpd"].function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "apigw" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  stage_name  = "test"
+}

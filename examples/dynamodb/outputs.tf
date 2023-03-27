af28a7be95a288cc8f270f853d7352d23cc38da4
Young-ook Kim <youngook.kim@gmail.com>
2023-03-27 19:58:05 +0900

    fix(blueprint): remove the out-dated apigw-lambda-ddb example

--- examples/dynamodb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lambda" {
-  description = "Attributes of lmabda function"
-  value       = module.lambda.function
-}
-
-output "log" {
-  description = "Attributes of cloudwatch log group for the lmabda function"
-  value       = module.lambda.logs
-}

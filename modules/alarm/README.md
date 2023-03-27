43f4e51ee6dc67247d204b55c21746e3f9f12991
youngookkim <youngook.kim@gmail.com>
2023-03-27 20:56:26 +0900

    fix(alarm): replace the alarm example with test cases (#70)
    * feat(alarm): cloudwatch alarm module test cases
    
    * docs(alarm): update the description of alarm module
    
    * fix(alarm): replace the alarm example with test cases

--- modules/alarm/README.md
+++ modules/alarm/README.md
@@ -1,12 +1,19 @@
 # AWS CloudWatch Alarm
+Amazon CloudWatch is basically a metrics repository. An AWS service—such as Amazon EC2—puts metrics into the repository, and you retrieve statistics based on those metrics. If you put your own custom metrics into the repository, you can retrieve statistics on these metrics as well.
 
 [AWS CloudWatch Alarms](https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html) enables you to trigger an alarm when systems encounters a specific situation.
+
+You can create both `metric` alarms and `composite` alarms in CloudWatch.
+
+- A metric alarm watches a single CloudWatch metric or the result of a math expression based on CloudWatch metrics. The alarm performs one or more actions based on the value of the metric or expression relative to a threshold over a number of time periods. The action can be sending a notification to an Amazon SNS topic, performing an Amazon EC2 action or an Auto Scaling action, or creating an OpsItem or incident in Systems Manager.
+
+- A composite alarm includes a rule expression that takes into account the alarm states of other alarms that you have created. The composite alarm goes into ALARM state only if all conditions of the rule are met. The alarms specified in a composite alarm's rule expression can include metric alarms and other composite alarms.
 
 ## Quickstart
 ### Setup
 ```hcl
 module "alarm" {
-  source  = "Young-ook/lambda/aws//modules/alarm"
+  source  = "Young-ook/eventbridge/aws//modules/alarm"
   name    = "example"
 }
 ```
---
+++ modules/alarm/tests/defaults/main.tf
@@ -0,0 +1,101 @@
+terraform {
+  required_providers {
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+  }
+}
+
+module "main" {
+  source = "../../"
+  for_each = { for a in [
+    {
+      name = "tc1"
+      alarm_metric = {
+        threshold = 1
+      }
+      metric_query = [
+        {
+          id          = "service_num_of_running_pods"
+          return_data = true
+          metric = [
+            {
+              metric_name = "CPUUtilization"
+              namespace   = "AWS/EC2"
+              period      = "60"
+              stat        = "Average"
+              dimensions = {
+                AutoScalingGroupName = "MyASG"
+              }
+            },
+          ]
+        },
+      ]
+    },
+    {
+      name = "tc2"
+      alarm_metric = {
+        # comparison_operator. following is supported:
+        #   GreaterThanOrEqualToThreshold
+        #   GreaterThanThreshold
+        #   LessThanThreshold
+        #   LessThanOrEqualToThreshold
+        comparison_operator = "LessThanThreshold"
+        datapoints_to_alarm = 1
+        evaluation_periods  = 1
+        threshold           = 1
+      }
+      metric_query = [
+        {
+          id          = "service_num_of_running_pods"
+          return_data = true
+          metric = [
+            {
+              metric_name = "service_number_of_running_pods"
+              namespace   = "ContainerInsights"
+              period      = "10"
+              stat        = "Average"
+              #        unit        = "Count"
+              dimensions = {
+                Namespace   = "sockshop"
+                Service     = "front-end"
+                ClusterName = "MyCluster"
+              }
+            },
+          ]
+        },
+      ]
+    },
+    {
+      name = "tc3"
+      alarm_metric = {
+        # comparison_operator. following is supported:
+        #   GreaterThanOrEqualToThreshold
+        #   GreaterThanThreshold
+        #   LessThanThreshold
+        #   LessThanOrEqualToThreshold
+        comparison_operator = "GreaterThanOrEqualToThreshold"
+        evaluation_periods  = 10
+      }
+      metric_query = [
+        {
+          id          = "elb_num_of_http502"
+          return_data = true
+          metric = [
+            {
+              metric_name = "HTTPCode_ELB_502_Count"
+              namespace   = "AWS/ApplicationELB"
+              period      = "60"
+              stat        = "Sum"
+              dimensions = {
+                LoadBalancer = "MyLB"
+              }
+            },
+          ]
+        },
+      ]
+    },
+  ] : a.name => a }
+  alarm_metric = lookup(each.value, "alarm_metric")
+  metric_query = lookup(each.value, "metric_query")
+}

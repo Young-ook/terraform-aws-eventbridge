37df8bd6a5070e36ec2e892ff1d4c6c52996db57
Young-ook Kim <youngook.kim@gmail.com>
2023-03-28 22:45:01 +0900

    fix(logs): remove the out-dated logs example

--- examples/logs/fixture.tc3.tfvars
+++
@@ -1,9 +0,0 @@
-aws_region = "ap-northeast-2"
-name       = "cwlogs-tc3-custom-config-with-ns"
-tags = {
-  env  = "dev"
-  test = "tc3"
-}
-log_group = {
-  retension_days = 5
-}
--- examples/logs/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "log" {
-  description = "Attributes of cloudwatch log group"
-  value       = module.logs
-}
-
-output "alarm" {
-  description = "CloudWatch alarm by log filtering"
-  value       = module.alarm
-}

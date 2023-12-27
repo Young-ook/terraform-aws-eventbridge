b6359a7af5a2f79b3a1a79b13b3f9efe9bae11eb
youngookkim <youngook.kim@gmail.com>
2023-12-27 09:14:53 +0900

    feat(lambda): update the test case for testing vpc and efs (#89)

--- modules/lambda/tests/vpc/main.tf
+++ modules/lambda/tests/vpc/main.tf
@@ -6,16 +6,10 @@ terraform {
   }
 }
 
-resource "random_pet" "petname" {
-  for_each  = toset(["sg", ])
-  length    = 3
-  separator = "-"
-}
-
 ### network/vpc
 module "vpc" {
   source  = "Young-ook/vpc/aws"
-  version = "1.0.3"
+  version = "1.0.6"
   vpce_config = [
     {
       service             = "s3"
@@ -40,29 +34,33 @@ module "vpc" {
   ]
 }
 
-### security/firewall
-resource "aws_security_group" "lambda" {
-  name        = random_pet.petname["sg"].id
-  description = format("security group for lambda function")
-  vpc_id      = module.vpc.vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [module.vpc.vpc.cidr_block]
+### filesystem
+module "efs" {
+  source  = "Young-ook/sagemaker/aws//modules/efs"
+  version = "0.4.6"
+  vpc     = module.vpc.vpc.id
+  subnets = values(module.vpc.subnets["public"])
+  filesystem = {
+    encrypted = false
   }
+  access_points = [
+    {
+      uid         = "1001"
+      gid         = "1001"
+      permissions = "750"
+      path        = "/export/lambda"
+    }
+  ]
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "time_sleep" "wait_efs" {
+  depends_on      = [module.efs]
+  create_duration = "5s"
 }
 
 ### application/package
 data "archive_file" "lambda_zip_file" {
+  depends_on  = [time_sleep.wait_efs]
   output_path = join("/", [path.module, "lambda_handler.zip"])
   source_file = join("/", [path.module, "../lambda_function.py"])
   type        = "zip"
@@ -70,7 +68,8 @@ data "archive_file" "lambda_zip_file" {
 
 ### application/function
 module "main" {
-  source = "../.."
+  depends_on = [time_sleep.wait_efs]
+  source     = "../.."
   lambda = {
     handler          = "lambda_function.lambda_handler"
     package          = data.archive_file.lambda_zip_file.output_path
@@ -78,13 +77,22 @@ module "main" {
   }
   vpc = {
     subnets         = values(module.vpc.subnets["public"])
-    security_groups = [aws_security_group.lambda.id]
+    security_groups = [module.efs.security_group.id]
   }
+  filesystem = {
+    local_mount_path = "/mnt/data"
+    arn              = module.efs.ap.0.arn
+  }
+}
+
+resource "time_sleep" "wait_lambda" {
+  depends_on      = [module.main]
+  create_duration = "5s"
 }
 
 ### invoke
 data "aws_lambda_invocation" "invoke" {
-  depends_on    = [module.main]
+  depends_on    = [time_sleep.wait_lambda]
   function_name = module.main.function.function_name
   input = jsonencode({
     key1 = "value1"

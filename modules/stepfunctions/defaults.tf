a2eb36fefb95d82be10ef5a4f8d0375713fc6914
Young-ook Kim <youngook.kim@gmail.com>
2024-01-11 15:21:30 +0900

    feat(sfn): enable to invoke lambda functions

--- modules/stepfunctions/defaults.tf
+++ modules/stepfunctions/defaults.tf
@@ -1,5 +1,10 @@
 ### default variables
 
+### aws paritions
+module "aws" {
+  source = "Young-ook/spinnaker/aws//modules/aws-partitions"
+}
+
 locals {
   workflow = var.workflow == null ? {} : var.workflow
   tracing  = var.tracing == null ? {} : var.tracing

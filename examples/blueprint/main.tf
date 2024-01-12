9e1b261f185fb52bc481efba4dfe98406eec0217
Young-ook Kim <youngook.kim@gmail.com>
2024-01-12 11:55:35 +0900

    feat(blueprint): upgrade the eventbridge module to support rule-targets internally

--- examples/blueprint/main.tf
+++ examples/blueprint/main.tf
@@ -15,8 +15,13 @@ provider "aws" {
 # It can only be used on the default event bus. For more information, refer to the
 # AWS documentation Schedule Expressions for Rules:
 # https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
-locals {
-  event_rules = [
+
+### choreography/eventbus
+module "default-eventbus" {
+  source  = "Young-ook/eventbridge/aws"
+  version = "0.0.16"
+  name    = "default"
+  rules = [
     {
       name                = "scheduled_job"
       schedule_expression = "rate(5 minutes)"
@@ -26,70 +31,45 @@ locals {
       event_pattern = file("event-pattern-example.json")
     },
   ]
-}
-
-### choreography/eventbus
-module "default-eventbus" {
-  source  = "Young-ook/eventbridge/aws"
-  version = "0.0.15"
-  name    = "default"
-  rules   = local.event_rules
+  targets = [
+    {
+      name = "scheduled-job-lambda"
+      rule = "scheduled_job"
+      arn  = module.lambda["running"].function.arn
+    },
+    {
+      name = "pattern-event-sfn"
+      rule = "pattern_event"
+      arn  = module.sfn.states.arn
+    },
+  ]
 }
 
 module "custom-eventbus" {
   source  = "Young-ook/eventbridge/aws"
-  version = "0.0.15"
+  version = "0.0.16"
   name    = "custom-eventbus"
-  rules   = [element(local.event_rules, 1)]
-}
-
-### choreography/route
-resource "aws_cloudwatch_event_target" "sfn" {
-  for_each = { for e in local.event_rules : e.name => e }
-  rule     = module.default-eventbus.rules[each.key].name
-  arn      = module.sfn.states.arn
-  role_arn = aws_iam_role.invoke-sfn.arn
-}
-
-module "aws" {
-  source = "Young-ook/spinnaker/aws//modules/aws-partitions"
-}
-
-### security/policy
-resource "aws_iam_role" "invoke-sfn" {
-  name = join("-", [var.name == null ? "eda" : var.name, "invoke-sfn"])
-  tags = var.tags
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = format("events.%s", module.aws.partition.dns_suffix)
-      }
-    }]
-    Version = "2012-10-17"
-  })
-
-  inline_policy {
-    name = join("-", [var.name == null ? "eda" : var.name, "invoke-sfn"])
-    policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [{
-        Action   = ["states:StartExecution"]
-        Effect   = "Allow"
-        Resource = module.sfn.states.arn
-      }]
-    })
-  }
+  rules = [
+    {
+      name          = "pattern_event"
+      event_pattern = file("event-pattern-example.json")
+    },
+  ]
+  targets = [
+    {
+      name = "pattern-event-sfn"
+      rule = "pattern_event"
+      arn  = module.sfn.states.arn
+    },
+  ]
 }
 
 ### orchestration/flow
 module "sfn" {
-  source      = "Young-ook/eventbridge/aws//modules/stepfunctions"
-  version     = "0.0.15"
-  name        = var.name
-  tags        = var.tags
-  policy_arns = [aws_iam_policy.invoke-lambda.arn]
+  source  = "Young-ook/eventbridge/aws//modules/stepfunctions"
+  version = "0.0.16"
+  name    = var.name
+  tags    = var.tags
   workflow = {
     definition = <<EOF
 {
@@ -105,19 +85,6 @@ module "sfn" {
 }
 EOF
   }
-}
-
-resource "aws_iam_policy" "invoke-lambda" {
-  name = join("-", [var.name == null ? "eda" : var.name, "invoke-lambda"])
-  tags = var.tags
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action   = ["lambda:InvokeFunction"]
-      Effect   = "Allow"
-      Resource = module.lambda["running"].function.arn
-    }]
-  })
 }
 
 ### application/package
